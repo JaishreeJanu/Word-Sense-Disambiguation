f3e8444084cd1175db0f18268d1ad587f00fbae6
janujaishree <janujaishree94@gmail.com>
2022-09-06 11:54:32 +0200

    sbert

---
+++ dist_flair.py
@@ -0,0 +1,167 @@
+"""
+Implementation of distributional lesk with flair embeddings
+"""
+#import nltk
+##nltk.download('wordnet')
+#nltk.download('omw-1.4')
+from nltk.corpus import wordnet as wn
+
+from numpy.linalg import norm
+import pickle as pk
+
+from loader import *
+from helper import *
+from flair.embeddings import WordEmbeddings, FlairEmbeddings, StackedEmbeddings
+from sklearn.decomposition import PCA
+from flair.data import Sentence
+import numpy as np
+from tqdm import tqdm
+
+SEMCOR_DATA_FILE = 'SemCor/semcor.data.xml'
+SEMCOR_LABELLED = 'SemCor/semcor.gold.key.txt'
+SENSEVAL_2_DATA_FILE = './senseval2/senseval2.data.xml'
+SENSEVAL_2_LABELLED = './senseval2/senseval2.gold.key.txt'
+SENSEVAL_3_DATA_FILE = './senseval3/senseval3.data.xml'
+SENSEVAL_3_LABELLED = './senseval3/senseval3.gold.key.txt'
+
+## Just test sentences
+context = 'Influential people are more important.'
+gloss = 'having authority or ascendancy or influence'
+gloss_opposite = 'of extreme importance; vital to the resolution of a crisis'
+
+def def_flair_embed():
+    """
+    Initializes the stack of flair embeddings
+    """
+    # init Flair forward and backwards embeddings
+    flair_embedding_forward = FlairEmbeddings('news-forward')
+    flair_embedding_backward = FlairEmbeddings('news-backward')
+
+    # create a StackedEmbedding object that combines glove and forward/backward flair embeddings
+    stacked_embeddings = StackedEmbeddings([
+        flair_embedding_forward,
+        flair_embedding_backward,
+    ])
+    return stacked_embeddings
+
+def get_flair_embed(lst_strings, pca_trained):
+    """
+    Returns the flair embedding for the sentence
+    """
+    context_sentence = Sentence(' '.join(lst_strings))
+    stacked_embeddings = def_flair_embed()
+    stacked_embeddings.embed(context_sentence)
+
+    flair_embeds = []
+    for token in context_sentence:
+        flair_embeds.append(token.embedding.numpy())
+    flair_embeds = pca_trained.transform(flair_embeds)
+
+    ## Find mean of the embeddings
+    flair_embeds = np.mean(flair_embeds, axis=0)
+
+    return flair_embeds
+
+def train_pca(senseval_2_lemmas, new_dim = 300):
+    """
+    Train PCA model to transform flair embeddings to 300-dimensional
+    """
+    senseval_2_lemmas = dict(list(senseval_2_lemmas.items())[:400]) ## Only selected 400 examples
+
+    stacked_embeddings = def_flair_embed()
+    ## Get the training examples for PCA
+    pca_train_embeds = []
+    for lemma_id, wsd in tqdm(senseval_2_lemmas.items()):
+        context_sentence = Sentence(' '.join(wsd.context))
+        stacked_embeddings.embed(context_sentence)
+
+        for token in context_sentence:
+            pca_train_embeds.append(token.embedding.numpy())
+
+    pca_train_embeds = np.asarray(pca_train_embeds)
+    ## train PCA on 300-dim since we have lexeme embeds in 300-dim
+    pca = PCA(n_components=new_dim)
+    pca.fit(pca_train_embeds)
+
+    return pca
+
+def eval_dist_flair(lemmas, labels, mapping_dict, lexeme_embeds, pca_trained):
+    """
+    Evaluate distributional lesk with flair embeddings
+    """
+    correct_count = 0
+    total = len(labels)
+
+    for lemma_id, lemma_label in tqdm(labels.items()):
+        this_wsd_inst = lemmas[lemma_id]
+
+        ## Get the context embeds
+        context_embed = get_flair_embed(this_wsd_inst.context, pca_trained)
+        final_score = 0
+        final_synset_keys = ''
+        for synset in wn.synsets(this_wsd_inst.lemma):
+            ## Computations for this synset, lemma pair
+            ## Get the gloss embeds
+            gloss_embed = get_flair_embed(synset.definition().split(" "), pca_trained)
+            this_synset_key = ''
+            for synset_lemma in synset.lemmas():
+                this_synset_key += synset_lemma.key()
+                this_synset_key += ','
+            this_synset_key = this_synset_key[:-1]  # Remove the last comma
+
+            #print(this_synset_key)
+            ## Get the wn-id from mapping.txt and using above dictionary
+            try:
+                print(this_synset_key)
+                wn_synset_id = mapping_dict[this_synset_key]
+                #wn_synset_id = 'wn-2.1-01953472-a'
+                ## Get the lexeme embeds using wn_id
+                lexeme_embed = lexeme_embeds[wn_synset_id]
+                ## Find similarity score for each word, sense pair
+                score = np.dot(lexeme_embed, context_embed) / (norm(lexeme_embed) * norm(context_embed)) + \
+                        np.dot(gloss_embed, context_embed) / (norm(gloss_embed) * norm(context_embed))
+
+
+                if score > final_score:
+                    final_score = score
+                    final_synset_keys = this_synset_key
+
+            except: pass
+
+        ## Write the code for finding the maximum score
+        print("The maximum score is:", final_score)
+        print("The predicted synset keys are:", final_synset_keys)
+
+        correct_label = labels[lemma_id][0]
+        pred_label = final_synset_keys.split(',')
+
+        for prediction in pred_label:
+            if correct_label == prediction:
+                correct_count += 1
+                break
+
+    return (correct_count / total) * 100
+
+if __name__ == '__main__':
+    semcor_lemmas = load_instances(SEMCOR_DATA_FILE)
+    semcor_labels = get_labels(SEMCOR_LABELLED)
+
+    senseval_2_lemmas = load_instances(SENSEVAL_2_DATA_FILE)
+    senseval_2_labels = get_labels(SENSEVAL_2_LABELLED)
+
+    senseval_3_lemmas = load_instances(SENSEVAL_3_DATA_FILE)
+    senseval_3_labels = get_labels(SENSEVAL_3_LABELLED)
+
+    mapping_dict = read_mapping()
+    lexeme_embeds = read_lexeme_embeds()
+
+    pca_trained = train_pca(senseval_2_lemmas)
+    pk.dump(pca_trained, open("pca_trained.pkl", "wb"))
+    pca_trained = pk.load(open("pca_trained.pkl", 'rb'))
+
+    senseval_2_acc = eval_dist_flair(senseval_3_lemmas, senseval_3_labels, mapping_dict, lexeme_embeds, pca_trained)
+
+    #print("Accuracy:: ", senseval_2_acc)
+
+
+
--- sbert_finetune.py
+++ sbert_finetune.py
@@ -1,42 +1,107 @@
 ### Fine tuning sbert and evluating distributional lesk on sbert sentence embeddings
 from loader import *
 from dict_utilities import *
+from helper import *
 
-from sentence_transformers import SentenceTransformer, InputExample, losses, util
+import gzip
+import csv
+import random
+import torch
+import os
+
+from sentence_transformers import SentenceTransformer, InputExample, losses, util, models
 from torch.utils.data import DataLoader
+from sklearn.decomposition import PCA
 
 import nltk
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 from nltk.corpus import wordnet as wn
+from numpy.linalg import norm
 
-SEMCOR_DATA_FILE = 'WSD_Unified_Evaluation_Datasets/SemCor/semcor.data.xml'
-SEMCOR_LABELLED = 'WSD_Unified_Evaluation_Datasets/SemCor/semcor.gold.key.txt'
+SEMCOR_DATA_FILE = 'SemCor/semcor.data.xml'
+SEMCOR_LABELLED = 'SemCor/semcor.gold.key.txt'
+SENSEVAL_2_DATA_FILE = './senseval2/senseval2.data.xml'
+SENSEVAL_2_LABELLED = './senseval2/senseval2.gold.key.txt'
+SENSEVAL_3_DATA_FILE = './senseval3/senseval3.data.xml'
+SENSEVAL_3_LABELLED = './senseval3/senseval3.gold.key.txt'
 
-def train_sbert(lemmas, model):
+## Just test sentences
+context = 'Influential people are more important.'
+gloss = 'having authority or ascendancy or influence'
+gloss_opposite = 'of extreme importance; vital to the resolution of a crisis'
+
+
+def train_pca(new_dimension):
+    """
+    We want to get a PCA matrix so as to reduce the dimensions of sbert embeddings
+    It returns the sbert model with pca added in dense layer
+    """
+    ## Get the sentences to compute PCA
+    nli_dataset_path = 'datasets/AllNLI.tsv.gz'
+    if not os.path.exists(nli_dataset_path):
+        util.http_get('https://sbert.net/datasets/AllNLI.tsv.gz', nli_dataset_path)
+
+    ## Define the model
+    #word_embedding_model = models.Transformer('bert-base-uncased', max_seq_length=256)
+    #pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
+
+    #model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+
+    # Read sentences from NLI dataset
+    nli_sentences = set()
+    with gzip.open(nli_dataset_path, 'rt', encoding='utf8') as fIn:
+        reader = csv.DictReader(fIn, delimiter='\t', quoting=csv.QUOTE_NONE)
+        for row in reader:
+            nli_sentences.add(row['sentence1'])
+            nli_sentences.add(row['sentence2'])
+
+    nli_sentences = list(nli_sentences)
+    random.shuffle(nli_sentences)
+
+    model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+
+    # To determine the PCA matrix, we need some example sentence embeddings.
+    # Here, we compute the embeddings for 20k random sentences from the AllNLI dataset
+    pca_train_sentences = nli_sentences[0:20000]
+    train_embeddings = model.encode(pca_train_sentences, convert_to_numpy=True)
+
+    # Compute PCA on the train embeddings matrix
+    pca = PCA(n_components=new_dimension)
+    pca.fit(train_embeddings)
+    pca_comp = np.asarray(pca.components_)
+
+    # Define the model. Either from scratch of by loading a pre-trained model
+
+    # We add a dense layer to the model, so that it will produce directly embeddings with the new size
+    dense = models.Dense(in_features=model.get_sentence_embedding_dimension(), out_features=new_dimension, bias=False,
+                         activation_function=torch.nn.Identity())
+    dense.linear.weight = torch.nn.Parameter(torch.tensor(pca_comp))
+    model.add_module('dense', dense)
+
+    return model
+
+def train_sbert(lemmas, labels, model):
     """
     Fine tune the sbert model on 5000 semcor sentences
     """
-    ## Fine tune sbert on 5000 of semcor instances
     train_examples = []
 
     for lemma_id, wsd_inst in lemmas.items():
-        label_key = semcor_labels[lemma_id][0]
+        label_key = labels[lemma_id][0]
         lemma = wsd_inst.lemma
         context = wsd_inst.context
-        gloss = wn.lemma_from_key(label_key).synset().definition()
-        gloss_opposite = ''
 
         for synset in wn.synsets(lemma):
-            for synset_lemma in synset.lemmas():
-                this_synset_key = synset_lemma.key()
-                if this_synset_key != label_key:
-                    gloss_opposite = synset.definition()
-                    break
-            if gloss_opposite != '': break
+            if wn.lemma_from_key(label_key).synset() == synset:
+                correct_gloss = synset.definition()
+                train_examples += [InputExample(texts=[context, correct_gloss], label=0.95)]
+            else:
+                incorrect_gloss = synset.definition()
+                train_examples += [InputExample(texts=[context, incorrect_gloss], label=0.15)]
 
-        train_examples += [InputExample(texts=[context, gloss], label=0.95),
-                           InputExample(texts=[context, gloss_opposite], label=0.25)]
+        #train_examples += [InputExample(texts=[context, gloss], label=0.95),
+                           #InputExample(texts=[context, gloss_opposite], label=0.25)]
 
     # Define your train dataset, the dataloader and the train loss
     train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
@@ -44,26 +109,103 @@ def train_sbert(lemmas, model):
 
     # Tune the model
     model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=1, warmup_steps=100)
-    model.save(path="sbert_model/", model_name='semcor_finetuned')
+    model.save(path="bert_multi_mini/")
 
     return model
+
+def eval_sbert(lemmas, labels, model, mapping_dict, lexeme_embeds):
+    """
+    Evaluation function for distributional lesk with sbert embeddings
+    """
+    correct_count = 0
+    total = len(labels)
+
+    for lemma_id, lemma_label in labels.items():
+        this_wsd_inst = lemmas[lemma_id]
+
+        ## Get the context embeds
+        context_embed = model.encode(' '.join(this_wsd_inst.context)) ## Since context is in list of strings format, join it to make a single string of a sentence
+        final_score = 0
+        final_synset_keys = ''
+        for synset in wn.synsets(this_wsd_inst.lemma):
+            ## Computations for this synset, lemma pair
+            ## Get the gloss embeds
+            gloss_embed = model.encode(synset.definition())
+            this_synset_key = ''
+            for synset_lemma in synset.lemmas():
+                this_synset_key += synset_lemma.key()
+                this_synset_key += ','
+            #this_synset_key = this_synset_key[:-1]  # Remove the last comma
+
+            try:
+                ## Get the wn-id from mapping.txt and using above dictionary
+                wn_synset_id = mapping_dict[this_synset_key]
+                ## Get the lexeme embeds using wn_id
+                lexeme_embed = lexeme_embeds[wn_synset_id]
+                ## Find similarity score for each word, sense pair
+                score = np.dot(gloss_embed, context_embed) / (norm(gloss_embed) * norm(context_embed)) + \
+                        np.dot(lexeme_embed, context_embed) / (norm(lexeme_embed) * norm(context_embed))
+
+                if score > final_score:
+                    final_score = score
+                    final_synset_keys = this_synset_key
+            except: pass
+
+        ## Write the code for finding the maximum score
+        print("The maximum score is:", final_score)
+        print("The predicted synset keys are:", final_synset_keys)
+
+
+        correct_label = labels[lemma_id][0]
+        pred_label = final_synset_keys.split(',')
+
+        for prediction in pred_label:
+            if correct_label == prediction:
+                correct_count += 1
+                break
+
+    return (correct_count / total) * 100
 
 
 if __name__ == '__main__':
     semcor_lemmas = load_instances(SEMCOR_DATA_FILE)
     semcor_labels = get_labels(SEMCOR_LABELLED)
 
-    # Define the model. Either from scratch of by loading a pre-trained model
-    model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
-    semcor_5000_lemmas = get_semcor_data(semcor_lemmas)
+    senseval_2_lemmas = load_instances(SENSEVAL_2_DATA_FILE)
+    senseval_2_labels = get_labels(SENSEVAL_2_LABELLED)
 
-    model = train_sbert(semcor_5000_lemmas, model)
+    senseval_3_lemmas = load_instances(SENSEVAL_3_DATA_FILE)
+    senseval_3_labels = get_labels(SENSEVAL_3_LABELLED)
+
+    semcor_5000_lemmas = get_semcor_data(semcor_lemmas)
+    ## Get the PCA trained model
+    model = train_pca(new_dimension=300) ## SInce the lexeme embeddings are 300-dimensional
+
+    ## Train the model
+    model = train_sbert(senseval_2_lemmas, senseval_2_labels, model)
+
+    ## Load the saved model
+    model = SentenceTransformer('./bert_multi_mini/')
 
     ## Test the model
-    query_embedding = model.encode('This framework generates embeddings for each input sentence')
-    passage_embedding = model.encode(['Sentences are passed as a list of string.',
-                                      'The quick brown fox jumps over the lazy dog.'])
-
+    query_embedding = model.encode(context)
+    passage_embedding = model.encode([gloss, gloss_opposite])
     print("Similarity: ", util.dot_score(query_embedding, passage_embedding))
+
+    #print("**********************************************************************")
+
+    ## Driver code for evaluating dist lesk with sbert embeddings
+    mapping_dict = read_mapping()
+    lexeme_embeds = read_lexeme_embeds()
+    #synset_embeds = read_synset_embeds()
+
+    #sem_accuracy = eval_sbert(semcor_lemmas, semcor_labels, model, mapping_dict, lexeme_embeds)
+    #print("Dist_sbert_lesk accuracy on Semcor:", sem_accuracy)
+
+    senseval_2_accuracy = eval_sbert(senseval_2_lemmas, senseval_2_labels, model, mapping_dict, lexeme_embeds)
+    print("Dist_lesk accuracy on Senseval_2:", senseval_2_accuracy)
+
+    #senseval_3_accuracy = eval_sbert(senseval_3_lemmas, senseval_3_labels, model, mapping_dict, lexeme_embeds)
+    #print("Dist_lesk accuracy on Senseval_3:", senseval_3_accuracy)
 
 
